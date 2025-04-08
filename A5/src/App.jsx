a2bfe52b6ad11339cf8e8e3330a10aa173fe58f2
Eliott <eliott.tenn@student.tdsb.on.ca>
2025-04-08 13:49:41 -0400

    Header added

--- A5/src/App.jsx
+++ A5/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
+// import AxiosEg from './components/AxiosExample'
+import Header from './components/header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_TMDB_KEY);
 
-  return (
-    <div>
-    </div>
+  return(
+    // <AxiosEg />
+    <Header />
   )
+
+
+  // const [count, setCount] = useState(0)
+  // console.log(import.meta.env.VITE_TMDB_KEY);
+
+  // return (
+  //   <div>
+  //   </div>
+  // )
 }
 
 export default App
---
+++ A5/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import './Header.css'
+
+function Header(){
+
+    return(
+        <div className="top">
+        <div className="logo">
+            <h2 className="title">10//10 Films</h2>
+        </div>
+        <div className="menu">
+            <ul className="options">
+                <li>Movies </li>
+                <li>TV Series </li>
+                <li>Most Popular</li>
+            </ul>
+        </div>
+        <div className="login">
+            <button className="loginButton">Login</button>
+            <button className="registerButton">Register</button>
+        </div>
+    </div>
+    )
+}
+
+export default Header;
